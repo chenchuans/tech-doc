8070ef79fc5093182a4f2f4def59d0e04ac5fefc
chenchuan <1350581922@qq.com>
2020-03-21 14:28:12 +0800

    更新

--- 排序.js
+++ 排序.js
@@ -176,3 +176,46 @@ function quickSort1(nums) {
   }
   recursive(nums, 0, nums.length - 1);
 }
+
+// 归并排序
+console.log(333, mergeSort[22, 3, 12, 1])
+function mergeSort(nums) {
+  if (!Array.isArray(nums)) {
+    return new TypeError('not a array');
+  }
+  if (nums.length < 2) {
+    return nums;
+  }
+  // 有序合并两个数组
+  function merge(l1, r1, l2, r2) {
+    let array = [];
+    let index = 0;
+    let i = l1, j = l2;
+    while (i <= r1 && j <= r2) {
+      array[index++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
+    }
+    while (i <= r1) {
+      array[index++] = nums[i++];
+    }
+    while (j <= r2) {
+      array[index++] = nums[j++];
+    }
+    // 将有序合并后的数组修改回原数组
+    for(let t = 0; t < index; t++) {
+      nums[l1 + t] = array[t];
+    }
+  }
+  // 递归将数组分为两个序列
+  function recursive(left, right) {
+    if (left >= right) {
+      return;
+    }
+    // 比起(left+right)/2，更推荐下面这种写法，可以避免数溢出
+    let mid = parseInt((right - left) / 2) + left;
+    recursive(left, mid);
+    recursive(mid + 1, right);
+    merge(left, mid, mid + 1, right);
+    return nums;
+  }
+  recursive(0, nums.length - 1);
+}
